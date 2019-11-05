c391288e3776e4735740a934c8d024e26220d1c8
Monica Hart <monica.hart@vml.com>
2019-11-05 14:40:14 -0600

    Adding in resulted object commands for CV client

--- ExploreCalifornia.cs
+++ ExploreCalifornia.cs
@@ -9,17 +9,28 @@ using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 using System.Threading.Tasks;
 using System.Linq;
+using ExploreCaliforniaFunction;
+using Microsoft.Extensions.Logging;
 
 namespace Company.Function
 {
     public static class ExploreCaliforniaFunction
     {
         [FunctionName("ExploreCalifornia")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        public async static Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, TraceWriter log)
         {
             var computerVisionClient = new ComputerVisionClient(new ApiKeyServiceClientCredentials(Constants.ComputerVisionApiKey));
             computerVisionClient.Endpoint = Constants.ComputerVisionEndpoint;
+
+            var imageStream = req.Body;
+
+            var result = await computerVisionClient.AnalyzeImageInStreamAsync(imageStream, details: new [] { Details.Landmarks });
+            var landmark = result.Categories.FirstOrDefault(c => c.Detail != null && c.Detail.Landmarks.Any());
+
+            return landmark != null ? (ActionResult)new OkObjectResult($"I think I see the {landmark.Detail.Landmarks.First().Name}")
+                    : new BadRequestObjectResult("No landmark was found in this image");
+            
         }
     }
 }
