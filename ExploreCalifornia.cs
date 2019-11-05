94682d55761cdf39740f70510b5e6991786f586c
Monica Hart <monica.hart@vml.com>
2019-11-05 14:12:49 -0600

    Changing file name for example

---
+++ ExploreCalifornia.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections;
+
+namespace Company.Function
+{
+    public static class ExploreCaliforniaFunction
+    {
+        [FunctionName("ExploreCalifornia")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            var computerVisionClient = new ComputerVisionClient(new ApiKeyServiceClientCredentials());
+        }
+    }
+}
