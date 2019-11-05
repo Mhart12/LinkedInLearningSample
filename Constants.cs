0706c82dbe191e6342e2ca69ba59720ac07bd17d
Monica Hart <monica.hart@vml.com>
2019-11-05 14:24:57 -0600

    Adding in Constants class

---
+++ Constants.cs
@@ -0,0 +1,11 @@
+namespace ExploreCaliforniaFunction
+{
+    public static class Constants
+    {
+        // TODO: Add your API key here
+        public const string ComputerVisionApiKey = "de6a4a040a6a45caadc3640c02cf5577";
+
+        // TODO: Add the right endpoint URL here
+        public const string ComputerVisionEndpoint = "https://bookievisionapi.cognitiveservices.azure.com/";
+    }
+}
